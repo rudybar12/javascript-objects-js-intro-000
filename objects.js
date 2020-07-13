23cdddd9307b72ee8d0db77228620c1ef64c4ef8
rodolfo Barrientos <rudybar12@yahoo.com>
2020-07-13 06:45:47 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = { song: 'tupac'}
 
 function updatePlaylist(playlist, artistName, songTitle){
-  return object.assign({}, playlist,{[artist:songTitle]})
+  return object.assign({}, playlist,{[artistName:songTitle]})
 }
