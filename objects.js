7cacd6e1864c4a839f70f1fc652af78d0723eac8
rodolfo Barrientos <rudybar12@yahoo.com>
2020-07-13 06:54:00 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,6 @@
 var playlist = { song: 'title'}
 
-var updatePlaylist = function (playlist, artistName, songTitle){
-  return Object.assign({}, playlist, {[artistName]: songTitle})
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle
+  return playlist
 }
